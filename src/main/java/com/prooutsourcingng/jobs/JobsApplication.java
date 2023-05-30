4ca026d5716a6f8f4ea67689abd70ad0bbbab54b
Joshua Chinwendu <jchinwendu@capitalbancorp.com>
2023-05-30 11:56:26 +0100

    Enabled CORS access on the JobsApplication.java file

--- src/main/java/com/prooutsourcingng/jobs/JobsApplication.java
+++ src/main/java/com/prooutsourcingng/jobs/JobsApplication.java
@@ -2,6 +2,9 @@ package com.prooutsourcingng.jobs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JobsApplication {
@@ -10,4 +13,14 @@ public class JobsApplication {
 		SpringApplication.run(JobsApplication.class, args);
 	}
 
+    @Bean
+    public WebMvcConfigurer configure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+//                WebMvcConfigurer.super.addCorsMappings(registry);
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
